e2af9537fcd03668dc7a173767c153adbe04a74e
Kevin Sven Lentz <kevin.s.lentz@web.de>
2022-11-18 07:56:53 +0100

    longer background

--- classes/world.class.js
+++ classes/world.class.js
@@ -11,9 +11,33 @@ class World{
     backgroundObjects = [ 
         new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 0),
         new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L1.png', 0),
-        
         new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L1.png', 0),
-       new BackgroundObject('img/3. Background/Layers/2. Floor/L1.png', 0)
+       new BackgroundObject('img/3. Background/Layers/2. Floor/L1.png', 0),
+
+       new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 719),
+       new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L2.png', 719),
+       new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L2.png', 719),
+      new BackgroundObject('img/3. Background/Layers/2. Floor/L2.png', 719),
+
+      new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 719*2),
+      new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L1.png', 719*2),
+      new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L1.png', 719*2),
+     new BackgroundObject('img/3. Background/Layers/2. Floor/L1.png', 719*2),
+
+     new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 719*3),
+     new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L2.png', 719*3),
+     new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L2.png', 719*3),
+    new BackgroundObject('img/3. Background/Layers/2. Floor/L2.png', 719*3),
+
+    new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 719*4),
+    new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L2.png', 719*4),
+    new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L2.png', 719*4),
+   new BackgroundObject('img/3. Background/Layers/2. Floor/L2.png', 719*4),
+
+   new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 719*5),
+   new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L2.png', 719*5),
+   new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L2.png', 719*5),
+  new BackgroundObject('img/3. Background/Layers/2. Floor/L2.png', 719*5)
     ]
     keyboard;
     camera_x = 0;
