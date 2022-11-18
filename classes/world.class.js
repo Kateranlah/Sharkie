50dc086292b44900d2b94f5d5822f962eda2c1c9
Kevin Sven Lentz <kevin.s.lentz@web.de>
2022-11-18 08:05:30 +0100

    added level class

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,44 +1,10 @@
 class World{
     character = new Character();
-    enemies = [
-        new Jellyfish(),
-        new Jellyfish(),
-        new Jellyfish()
-     ];
+    enemies = level1.enemies;
     canvas;
     ctx;
-    lights = [new Light()]
-    backgroundObjects = [ 
-        new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 0),
-        new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L1.png', 0),
-        new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L1.png', 0),
-       new BackgroundObject('img/3. Background/Layers/2. Floor/L1.png', 0),
-
-       new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 719),
-       new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L2.png', 719),
-       new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L2.png', 719),
-      new BackgroundObject('img/3. Background/Layers/2. Floor/L2.png', 719),
-
-      new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 719*2),
-      new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L1.png', 719*2),
-      new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L1.png', 719*2),
-     new BackgroundObject('img/3. Background/Layers/2. Floor/L1.png', 719*2),
-
-     new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 719*3),
-     new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L2.png', 719*3),
-     new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L2.png', 719*3),
-    new BackgroundObject('img/3. Background/Layers/2. Floor/L2.png', 719*3),
-
-    new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 719*4),
-    new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L2.png', 719*4),
-    new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L2.png', 719*4),
-   new BackgroundObject('img/3. Background/Layers/2. Floor/L2.png', 719*4),
-
-   new BackgroundObject('img/3. Background/Layers/5. Water/L1.png', 719*5),
-   new BackgroundObject('img/3. Background/Layers/3.Fondo 1/L2.png', 719*5),
-   new BackgroundObject('img/3. Background/Layers/4.Fondo 2/L2.png', 719*5),
-  new BackgroundObject('img/3. Background/Layers/2. Floor/L2.png', 719*5)
-    ]
+    lights = level1.lights
+    backgroundObjects= level1.backgroundObjects;
     keyboard;
     camera_x = 0;
     
---
+++ levels/level1.js
@@ -0,0 +1,35 @@
+const level1 = new Level(
+  [new Jellyfish(), new Jellyfish(), new Jellyfish()],
+  [new Light()],
+  [
+    new BackgroundObject("img/3. Background/Layers/5. Water/L1.png", 0),
+    new BackgroundObject("img/3. Background/Layers/3.Fondo 1/L1.png", 0),
+    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/L1.png", 0),
+    new BackgroundObject("img/3. Background/Layers/2. Floor/L1.png", 0),
+
+    new BackgroundObject("img/3. Background/Layers/5. Water/L1.png", 719),
+    new BackgroundObject("img/3. Background/Layers/3.Fondo 1/L2.png", 719),
+    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/L2.png", 719),
+    new BackgroundObject("img/3. Background/Layers/2. Floor/L2.png", 719),
+
+    new BackgroundObject("img/3. Background/Layers/5. Water/L1.png", 719 * 2),
+    new BackgroundObject("img/3. Background/Layers/3.Fondo 1/L1.png", 719 * 2),
+    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/L1.png", 719 * 2),
+    new BackgroundObject("img/3. Background/Layers/2. Floor/L1.png", 719 * 2),
+
+    new BackgroundObject("img/3. Background/Layers/5. Water/L1.png", 719 * 3),
+    new BackgroundObject("img/3. Background/Layers/3.Fondo 1/L2.png", 719 * 3),
+    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/L2.png", 719 * 3),
+    new BackgroundObject("img/3. Background/Layers/2. Floor/L2.png", 719 * 3),
+
+    new BackgroundObject("img/3. Background/Layers/5. Water/L1.png", 719 * 4),
+    new BackgroundObject("img/3. Background/Layers/3.Fondo 1/L2.png", 719 * 4),
+    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/L2.png", 719 * 4),
+    new BackgroundObject("img/3. Background/Layers/2. Floor/L2.png", 719 * 4),
+
+    new BackgroundObject("img/3. Background/Layers/5. Water/L1.png", 719 * 5),
+    new BackgroundObject("img/3. Background/Layers/3.Fondo 1/L2.png", 719 * 5),
+    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/L2.png", 719 * 5),
+    new BackgroundObject("img/3. Background/Layers/2. Floor/L2.png", 719 * 5),
+  ]
+);
