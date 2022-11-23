589a78357dad084c7b7ff028f631b99b783e79bd
Kevin Sven Lentz <kevin.s.lentz@web.de>
2022-11-23 08:53:30 +0100

    added heartclass

---
+++ classes/hearts.class.js
@@ -0,0 +1,16 @@
+class Hearts extends Collectables{
+
+    IMAGES = ["img/4. Marcadores/1. Coins/1.png",
+    "img/4. Marcadores/1. Coins/2.png",
+    "img/4. Marcadores/1. Coins/3.png",
+    "img/4. Marcadores/1. Coins/4.png"]
+
+
+    constructor(){
+        super();
+        this.loadImages(this.IMAGES)
+        this.x = 200 + Math.random() * 500;
+        this.y = 50 + Math.random() * 300;
+        this.animate(this.IMAGES);
+    }
+}
